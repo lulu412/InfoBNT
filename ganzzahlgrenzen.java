62bb212ee86ddc5fbee05db40cb2197135095a3c
lulu412 <saluk412@t-online.de>
2014-07-09 12:54:57 +0200

    Create ganzzahlgrenzen.java

---
+++ ganzzahlgrenzen.java
@@ -0,0 +1,18 @@
+public static void main(String[] args) {
+    byte b = 42;
+    short s = 42;
+    int i = 42;
+    long l = 42L;
+    System.out.println(b);
+    System.out.println(s);
+    System.out.println(i);
+    System.out.println(l);
+    System.out.println(Byte.MIN_VALUE);
+    System.out.println(Byte.MAX_VALUE);
+    System.out.println(Short.MIN_VALUE);
+    System.out.println(Short.MAX_VALUE);
+    System.out.println(Integer.MIN_VALUE);
+    System.out.println(Integer.MAX_VALUE);
+    System.out.println(Long.MIN_VALUE);
+    System.out.println(Long.MAX_VALUE);
+}
