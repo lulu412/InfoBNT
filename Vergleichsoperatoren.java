ca1ad3d0418077655db7c9aff08c5067712a3efc
lulu412 <saluk412@t-online.de>
2014-07-09 12:52:51 +0200

    Create Vergleichsoperatoren.java

---
+++ Vergleichsoperatoren.java
@@ -0,0 +1,16 @@
+/*
+
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor. */ package vergleichsoperatoren;
+/**
+*
+
+@author privat
+*/
+public class Vergleichsoperatoren {
+
+/**
+
+@param args the command line arguments */ public static void main(String[] args) { System.out.println(1 + 1 < 42); System.out.println(1 + 1 <= 42); System.out.println(1 + 1 > 42); System.out.println(1 + 1 >= 42); System.out.println(1 + 8 == 42); System.out.println(1 + 1 != 42); System.out.println(1 + 42 > 42); System.out.println(2 * 125 >300); }
+}
